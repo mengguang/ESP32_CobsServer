7fb7a42d7aca8b1b1a3eb33c06d80a5886d177cf
Meng Guang <mengguang@gmail.com>
2021-07-07 03:32:15 +0800

    wire and digital io works

---
+++ src/CobsMessage.h
@@ -0,0 +1,86 @@
+#pragma once
+
+#include "cobs.h"
+#include <cstdarg>
+#include <cstdio>
+#include <cstring>
+
+#define MAX_ENCODED_MESSAGE_SIZE 253
+#define MAX_DECODED_MESSAGE_SIZE 250
+
+class CobsMessage
+{
+public:
+    virtual bool message_send_writer(const uint8_t *data, uint32_t length) = 0;
+    virtual bool message_processor(const uint8_t *data, uint32_t length) = 0;
+    virtual void debug_message_writer(const char *message) = 0;
+
+    size_t debug_printf(const char *format, ...)
+    {
+        va_list argptr;
+        va_start(argptr, format);
+        int n = vsnprintf(debug_message, sizeof(debug_message), format, argptr);
+        va_end(argptr);
+        debug_message_writer(debug_message);
+        return n;
+    }
+
+    bool send_message(uint8_t *data, uint32_t data_length)
+    {
+        uint8_t encoded_message_buffer[MAX_ENCODED_MESSAGE_SIZE + 1];
+        memset(encoded_message_buffer, 0, sizeof(encoded_message_buffer));
+        cobs_encode_result result = cobs_encode(encoded_message_buffer, MAX_ENCODED_MESSAGE_SIZE, data, data_length);
+        if (result.status != COBS_ENCODE_OK)
+        {
+            debug_printf("message encode error: %d\n", result.status);
+            return false;
+        }
+        // Serial1.write(encoded_message_buffer, result.out_len + 1);
+        message_send_writer(encoded_message_buffer, (uint32_t)(result.out_len + 1));
+        return true;
+    }
+    bool send_message(const char *string_message)
+    {
+        return send_message((uint8_t *)string_message, (uint32_t)(strlen(string_message) + 1));
+    }
+    bool decode_and_process_message(uint8_t *encoded_message, uint32_t encoded_message_length)
+    {
+        uint8_t decoded_message_buffer[MAX_DECODED_MESSAGE_SIZE] = {0};
+        cobs_decode_result result = cobs_decode(decoded_message_buffer, sizeof(decoded_message_buffer), encoded_message, encoded_message_length);
+        if (result.status == COBS_DECODE_OK)
+        {
+            return message_processor(decoded_message_buffer, (uint32_t)(result.out_len));
+        }
+        else
+        {
+            debug_printf("message decode error: %d\n", result.status);
+            return false;
+        }
+    }
+    bool message_decoder_fill_data(uint8_t c)
+    {
+        if (c > 0)
+        {
+            if (n_received >= sizeof(received_message_buffer))
+            {
+                debug_printf("Error: receive buffer will overflow, abort.\n");
+                n_received = 0;
+                return false;
+            }
+            received_message_buffer[n_received] = c;
+            n_received++;
+            return true;
+        }
+        else
+        {
+            bool result = decode_and_process_message(received_message_buffer, n_received);
+            n_received = 0;
+            return result;
+        }
+    }
+
+private:
+    uint32_t n_received = 0;
+    uint8_t received_message_buffer[MAX_ENCODED_MESSAGE_SIZE];
+    char debug_message[250];
+};
---
+++ src/main.cpp
@@ -0,0 +1,170 @@
+#include <Arduino.h>
+#include "CobsMessage.h"
+#include "ArduinoCommand.h"
+#include <Wire.h>
+
+const int led = LED_BUILTIN;
+const int led2 = PA0;
+
+HardwareSerial SerialJlink(PB11, PB10);
+
+#define SerialDebug SerialJlink
+#define SerialComm Serial
+
+int command_processor(const uint8_t *message, uint32_t message_length, uint8_t *result, uint32_t result_length)
+{
+  uint8_t cmd = message[0];
+  int reply_length = 0;
+  switch (cmd)
+  {
+  case CMD_PIN_MODE:
+  {
+    pinMode(message[1], message[2]);
+    result[0] = 0x01;
+    reply_length = CMD_PIN_MODE_RL;
+    break;
+  }
+  case CMD_DIGITAL_WRITE:
+  {
+    digitalWrite(message[1], message[2]);
+    result[0] = 0x01;
+    reply_length = CMD_DIGITAL_WRITE_RL;
+    break;
+  }
+  case CMD_DIGITAL_READ:
+  {
+    int value = digitalRead(message[1]);
+    result[0] = 0x01;
+    result[1] = (uint8_t)value;
+    reply_length = CMD_DIGITAL_READ_RL;
+    break;
+  }
+  case CMD_WIRE_BEGIN:
+  {
+    uint8_t sda = message[1];
+    uint8_t scl = message[2];
+    Wire.begin(sda, scl);
+    result[0] = 0x01;
+    reply_length = CMD_WIRE_BEGIN_RL;
+    break;
+  }
+  case CMD_WIRE_REQUEST_FROM:
+  {
+    uint8_t address = message[1];
+    uint8_t n_read = message[2];
+    uint8_t ret = Wire.requestFrom(address, n_read);
+    result[0] = 0x01;
+    result[1] = ret; //read back length
+    if (ret > 0)
+    {
+      Wire.readBytes(result + 2, ret);
+    }
+    reply_length = 2 + ret;
+    break;
+  }
+  case CMD_WIRE_TRANSMISSION:
+  {
+    uint8_t address = message[1];
+    uint32_t data_length = message_length - 2;
+    Wire.beginTransmission(address);
+    if (data_length > 0)
+    {
+      Wire.write(message + 2, data_length);
+    }
+    uint8_t ret = Wire.endTransmission();
+    result[0] = 0x01;
+    result[1] = ret;
+    reply_length = CMD_WIRE_TRANSMISSION_RL;
+    break;
+  }
+
+  default:
+    break;
+  }
+  return reply_length;
+}
+
+void hex_dump(const uint8_t *data, uint32_t length)
+{
+  for (uint32_t i = 0; i < length; i++)
+  {
+    SerialDebug.printf("%02X ", data[i]);
+    if ((i + 1) % 16 == 0)
+    {
+      SerialDebug.printf("\n");
+    }
+  }
+  if (length % 16 != 0)
+  {
+    SerialDebug.printf("\n");
+  }
+}
+
+class SimpleCobsMessage : public CobsMessage
+{
+public:
+  void debug_message_writer(const char *message)
+  {
+    SerialDebug.print(message);
+  }
+  bool message_send_writer(const uint8_t *data, uint32_t length)
+  {
+    // SerialDebug.print("send back:\n");
+    // hex_dump(data, length);
+    int n = SerialComm.write(data, length);
+    SerialDebug.printf("write back size: %d\n", n);
+    return true;
+  }
+  bool message_processor(const uint8_t *data, uint32_t length)
+  {
+    SerialDebug.print("message: \n");
+    hex_dump(data, length);
+
+    uint8_t reply_buffer[256] = {0};
+    int reply_length = command_processor(data, length, reply_buffer, sizeof(reply_buffer));
+    if (reply_buffer <= 0)
+    {
+      debug_printf("command processor error: %d\n", reply_length);
+      return false;
+    }
+    SerialDebug.print("reply: \n");
+    hex_dump(reply_buffer, reply_length);
+    return send_message(reply_buffer, reply_length);
+    // return true;
+  }
+};
+
+SimpleCobsMessage messager;
+
+void setup()
+{
+  SerialDebug.begin(115200);
+  pinMode(led, OUTPUT);
+
+  SerialComm.begin(115200);
+}
+
+uint32_t last = millis();
+void loop()
+{
+  if (SerialComm.available())
+  {
+    int c = SerialComm.read();
+    if (c < 0)
+    {
+      SerialDebug.printf("Serial1 read error: %d\n", c);
+    }
+    else
+    {
+      // SerialDebug.printf("%02X\n", c);
+      messager.message_decoder_fill_data(c);
+    }
+    delay(1);
+  }
+  if (millis() > last + 500)
+  {
+    last = millis();
+    // digitalToggle(led);
+  }
+  delay(1);
+}
